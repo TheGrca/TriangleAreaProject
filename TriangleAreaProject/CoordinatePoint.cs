c7b2a995eeefdfb59e86f9ac1d41e57425e4b49a
Ivan Grcic <100207993+TheGrca@users.noreply.github.com>
2024-04-22 16:01:33 +0200

    Finished the project

---
+++ TriangleAreaProject/CoordinatePoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleAreaProject
+{
+    public class CoordinatePoint //Pomocna klasa koja nam cuva podatke o tackama u koordinatnom sistemu
+    {
+        public double X {  get; set; } //Property za X osu
+        public double Y { get; set; } // Property za Y osu
+
+        public CoordinatePoint(double x, double y) //Konstruktor za klasu koja za parametre prima X i Y koordinate
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
---
+++ TriangleAreaProject/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleAreaProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int pointNumber = 0;
+            bool isNumber = false;
+            bool tryAgain = true;
+            while (tryAgain)
+            {
+
+                while (!isNumber)
+                {
+                    Console.WriteLine("Insert a number of how many points you want to insert: ");
+                    if (int.TryParse(Console.ReadLine(), out pointNumber))
+                    {
+                        if (pointNumber <= 0)
+                        {
+                            Console.WriteLine("Point number cannot be a negative number nor zero!\n");
+                        }
+                        else
+                        {
+                            isNumber = true;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Point number must be a number!\n");
+                    }
+                }
+
+                CoordinatePoint[] points = new CoordinatePoint[pointNumber];
+
+                for (int i = 0; i < pointNumber; i++)
+                {
+                    int x = 0, y = 0;
+                    bool isValidPoint = false;
+                    while (!isValidPoint)
+                    {
+                        Console.WriteLine($"Insert the coordinates for the point {i + 1}: ");
+                        Console.WriteLine("X => ");
+                        if (int.TryParse(Console.ReadLine(), out x))
+                        {
+                            Console.WriteLine("Y => ");
+                            if (int.TryParse(Console.ReadLine(), out y))
+                            {
+                                isValidPoint = true;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid input for Y coordinate.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input for X coordinate.");
+                        }
+                    }
+                    points[i] = new CoordinatePoint(x, y);
+                }
+
+                FindLargestTriangle(points);
+                Console.WriteLine("Do you want to calculate again? (Type any key to continue or 'No' to exit!)");
+                string result = Console.ReadLine();
+                if (result.ToLower().Replace(" ", "").Equals("no")){
+                    tryAgain = false;
+                }
+            }
+        }
+
+        static void FindLargestTriangle(CoordinatePoint[] points)
+        {
+            double maxTriangleArea = 0;
+            CoordinatePoint A = null, B = null, C = null;
+
+            for(int i = 0; i < points.Length; i++)
+            {
+                for(int j = i + 1; j < points.Length; j++)
+                {
+                    for(int k = j + 1; k < points.Length; k++)
+                    {
+                        double triangleArea = GetTriangleArea(points[i], points[j], points[k]);
+                        if(triangleArea > maxTriangleArea)
+                        {
+                            maxTriangleArea = triangleArea;
+                            A = points[i];
+                            B = points[j];
+                            C = points[k];
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Largest triangle area: " + maxTriangleArea);
+            Console.WriteLine("Points: ");
+            Console.WriteLine("A: (" + A.X + ", " + A.Y + ")");
+            Console.WriteLine("B: (" + B.X + ", " + B.Y + ")");
+            Console.WriteLine("C: (" + C.X + ", " + C.Y + ")");
+        }
+
+        static double GetTriangleArea(CoordinatePoint A, CoordinatePoint B, CoordinatePoint C) { 
+            double P = Math.Abs((A.X * (B.Y - C.Y) + B.X * (C.Y - A.Y) + C.X * (A.Y - B.Y)) / 2);
+            return P;
+        }
+    }
+}
